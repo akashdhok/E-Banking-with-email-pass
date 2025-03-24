ac571a35affabd9ec9598450b1bd7ef2cd7d652b
Akash Dhok <akashdhok1265@gmail.com>
2025-03-24 13:17:25 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,19 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const mongoose = require("mongoose")
 require("dotenv").config()
 const Port = process.env.PORT
-const dbConnect = require("./utils/db")   //database connection form utils
+
 const costumerRoute = require("./routes/costumerRoute")
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended : true}))
 
+  mongoose.connect(process.env.DB_CONNECTION).then(()=>{
+            console.log("DB Connected")
+        })
 app.use("/customer" , costumerRoute)
-
-dbConnect()
-
 
 app.listen(Port , ()=>{
     console.log("Listening at the port of 8000")
